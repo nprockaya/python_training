6b8d7c9b1d9f7996308d8e8f0e5af3ce035d3b8a
nprockaya <41466864+nprockaya@users.noreply.github.com>
2021-02-10 20:01:34 +0300

    Final Version of Recordered Test v2

--- test_add_group.py
+++ test_add_group.py
@@ -1,11 +1,6 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
 
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
@@ -40,16 +35,6 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_link_text("group page").click()
         wd.find_element_by_link_text("Logout").click()
     
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-
     def tearDown(self):
         self.wd.quit()
 
