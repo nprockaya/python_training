583cff76a0031499e3dc9f197ebfec5c5cc42a64
nprockaya <41466864+nprockaya@users.noreply.github.com>
2021-02-11 23:56:33 +0300

    Added Birthday and Anniversary Day

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-# from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import Select
 from contact_class import Contact
 import unittest
 
@@ -15,7 +15,7 @@ class test_Add_Contact (unittest.TestCase):
         self.open_homepage(wd)
         self.login(wd, user_name="admin", password="secret")
         self.open_contact_creation_page(wd)
-        self.contact_creation(wd, Contact(first_name_value="Firstname", middle_name_value="Middlename", last_name_value="Lastname", nickname_value="Nickname", title_value="Title", company_value="Company", address_value="123456, address_city, address_street, address_home", home_phone_value="123456_home_phone", mobile_phone_value="123456_mobile_phone", work_phone_value="123456_work_phone", fax_value="123456_fax", email_value="1@mail.com", email2_value="2@mail.com", email3_value="3@mail.com", homepage_value="https://home_page.com", secondary_address_value="Second address", secondary_home_value="Second home", secondary_notes_value="Notes"))
+        self.contact_creation(wd, Contact(first_name_value="Firstname", middle_name_value="Middlename", last_name_value="Lastname", nickname_value="Nickname", title_value="Title", company_value="Company", address_value="123456, address_city, address_street, address_home", home_phone_value="123456_home_phone", mobile_phone_value="123456_mobile_phone", work_phone_value="123456_work_phone", fax_value="123456_fax", email_value="1@mail.com", email2_value="2@mail.com", email3_value="3@mail.com", homepage_value="https://home_page.com", bday_value="1", bmonth_value="January", byear_value="2000", aday_value="1", amonth_value="January", ayear_value="2020", secondary_address_value="Second address", secondary_home_value="Second home", secondary_notes_value="Notes"))
         # submit contact creation and return to home page
         self.submit_contact_creation(wd)
         self.return_to_homepage(wd)
@@ -55,8 +55,8 @@ class test_Add_Contact (unittest.TestCase):
         self.fill_email2(contact, wd)
         self.fill_email3(contact, wd)
         self.fill_homepage(contact, wd)
-       # self.fill_bday(wd)
-       # self.fill_aday(wd)
+        self.fill_bday(contact, wd)
+        self.fill_aday(contact, wd)
         self.fill_secondary_address(contact, wd)
         self.fill_secondary_home(contact, wd)
         self.fill_secondary_notes(contact, wd)
@@ -136,26 +136,27 @@ class test_Add_Contact (unittest.TestCase):
         wd.find_element_by_name("homepage").clear()
         wd.find_element_by_name("homepage").send_keys(contact.homepage)
 
-    # def fill_bday(self, wd):
-    #    wd.find_element_by_name("bday").click()
-    #    Select(wd.find_element_by_name("bday")).select_by_visible_text("1")
-    #    wd.find_element_by_xpath("//option[@value='1']").click()
-    #    wd.find_element_by_name("bmonth").click()
-    #    Select(wd.find_element_by_name("bmonth")).select_by_visible_text("January")
-    #    wd.find_element_by_xpath("//option[@value='January']").click()
-    #    wd.find_element_by_name("byear").click()
-    #    wd.find_element_by_name("byear").clear()
-    #    wd.find_element_by_name("byear").send_keys("2000")
+    def fill_bday(self, contact, wd):
+        wd.find_element_by_name("bday").click()
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.bday)
+        wd.find_element_by_xpath("//option[@value='" + contact.bday + "']").click()
+        wd.find_element_by_name("bmonth").click()
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.bmonth)
+        wd.find_element_by_xpath("//option[@value='" + contact.bmonth + "']")
+        wd.find_element_by_name("byear").click()
+        wd.find_element_by_name("byear").clear()
+        wd.find_element_by_name("byear").send_keys(contact.byear)
 
-    # def fill_aday(self, wd):
-    #    Select(wd.find_element_by_name("aday")).select_by_visible_text("2")
-    #    wd.find_element_by_xpath("(//option[@value='2'])[2]").click()
-    #    wd.find_element_by_name("amonth").click()
-    #    Select(wd.find_element_by_name("amonth")).select_by_visible_text("January")
-    #    wd.find_element_by_xpath("(//option[@value='January'])[2]").click()
-    #    wd.find_element_by_name("ayear").click()
-    #    wd.find_element_by_name("ayear").clear()
-    #    wd.find_element_by_name("ayear").send_keys("2020")
+    def fill_aday(self, contact, wd):
+        wd.find_element_by_name("aday").click()
+        Select(wd.find_element_by_name("aday")).select_by_visible_text(contact.aday)
+        wd.find_element_by_xpath("//option[@value='" + contact.aday + "']").click()
+        wd.find_element_by_name("amonth").click()
+        Select(wd.find_element_by_name("amonth")).select_by_visible_text(contact.amonth)
+        wd.find_element_by_xpath("//option[@value='" + contact.amonth + "']").click()
+        wd.find_element_by_name("ayear").click()
+        wd.find_element_by_name("ayear").clear()
+        wd.find_element_by_name("ayear").send_keys(contact.ayear)
 
     def fill_secondary_address(self, contact, wd):
         wd.find_element_by_name("address2").click()
