5c26a6d4440d2a1199d9be8b5310234ff109b282
nprockaya <41466864+nprockaya@users.noreply.github.com>
2021-02-22 15:21:36 +0300

    Bug-fix in session.py

--- Fixtures/session.py
+++ Fixtures/session.py
@@ -1,5 +1,3 @@
-import time
-
 class SessionHelper:
 
     def __init__(self, app):
@@ -19,5 +17,5 @@ class SessionHelper:
     def logout(self):
         wd = self.app.wd
         wd.find_element_by_link_text("Logout").click()
-        time.sleep(0.1)
+        wd.find_element_by_name("user")
 
