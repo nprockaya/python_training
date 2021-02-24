de1b14fa5863802c51b0a759773fb4af883b8b0f
nprockaya <41466864+nprockaya@users.noreply.github.com>
2021-02-25 00:11:53 +0300

    Промежуточный результат

--- Fixtures/application.py
+++ Fixtures/application.py
@@ -22,3 +22,10 @@ class Application:
 
     def destroy(self):
         self.wd.quit()
+
+    def is_valid(self):
+        try:
+            self.wd.current_url()
+            return True
+        except:
+            return False
--- conftest.py
+++ conftest.py
@@ -1,12 +1,24 @@
 from Fixtures.application import Application
 import pytest
 
+fixture = None
+
+
+@pytest.fixture()
+def app():
+    global fixture
+    if fixture is None:
+        fixture = Application()
+        fixture.session.login(user_name="admin", password="secret")
+    else:
+        if not fixture.is_valid():
+            fixture = Application()
+            fixture.session.login(user_name="admin", password="secret")
+    return fixture
+
 
 @pytest.fixture(scope="session", autouse=True)
-def app(request):
-    fixture = Application()
-    fixture.session.login(user_name="admin", password="secret")
-
+def stop(request):
     def final():
         fixture.session.ensure_logout()
         fixture.destroy()
