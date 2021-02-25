5adb071f4fbe68c37b67bf613501b6a857b5745d
nprockaya <41466864+nprockaya@users.noreply.github.com>
2021-02-25 23:57:35 +0300

    Задание №9

--- Fixtures/contact.py
+++ Fixtures/contact.py
@@ -181,7 +181,8 @@ class ContactHelper:
 
     def return_to_homepage(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("home").click()
+        if not (wd.current_url.endswith("/addressbook/") and len(wd.find_elements_by_id("search_count")) > 0):
+            wd.find_element_by_link_text("home").click()
 
     def select_first_contact(self):
         wd = self.app.wd
--- Fixtures/group.py
+++ Fixtures/group.py
@@ -33,7 +33,8 @@ class GroupHelper:
 
     def return_to_groups_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("groups").click()
+        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("New")) > 0):
+            wd.find_element_by_link_text("groups").click()
 
     def select_first_group(self):
         wd = self.app.wd
