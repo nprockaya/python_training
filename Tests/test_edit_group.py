ec514fc95937c05f130d8a76cb09c2b4755ad4f2
nprockaya <41466864+nprockaya@users.noreply.github.com>
2021-02-25 17:59:12 +0300

    Добавлены проверки наличия группы перед редактированием

--- Tests/test_edit_group.py
+++ Tests/test_edit_group.py
@@ -8,15 +8,21 @@ from Models.group_class import Group
 #     app.session.logout()
 
 def test_edit_first_group_name(app):
+    if app.group.count() == 0:
+        app.group.create(Group(_name="for_edit_test", _header="", _footer=""))
     app.group.edit_first_group(
         Group(_name="name_group_edit_test"))
 
 
 def test_edit_first_group_header(app):
+    if app.group.count() == 0:
+        app.group.create(Group(_name="", _header="for_edit_test", _footer=""))
     app.group.edit_first_group(
         Group(_header="header_group_edit_test"))
 
 
 def test_edit_first_group_footer(app):
+    if app.group.count() == 0:
+        app.group.create(Group(_name="", _header="", _footer="for_edit_test"))
     app.group.edit_first_group(
         Group(_footer="footer_group_edit_test"))
