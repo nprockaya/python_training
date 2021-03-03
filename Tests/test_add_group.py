022a8aa0bd30f5620fa32adaee6ff0f5cdfeb6fc
nprockaya <41466864+nprockaya@users.noreply.github.com>
2021-03-03 22:45:20 +0300

    Промежуточное выполнение, апдейт тестов

--- Tests/test_add_group.py
+++ Tests/test_add_group.py
@@ -8,5 +8,9 @@ def test_add_normal_group(app):
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
 
+
 def test_add_empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(_name="", _header="", _footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
--- Tests/test_edit_group.py
+++ Tests/test_edit_group.py
@@ -10,19 +10,28 @@ from Models.group_class import Group
 def test_edit_first_group_name(app):
     if app.group.count_groups() == 0:
         app.group.create(Group(_name="for_edit_test", _header="", _footer=""))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(
         Group(_name="name_group_edit_test"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_edit_first_group_header(app):
     if app.group.count_groups() == 0:
         app.group.create(Group(_name="", _header="for_edit_test", _footer=""))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(
         Group(_header="header_group_edit_test"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_edit_first_group_footer(app):
     if app.group.count_groups() == 0:
         app.group.create(Group(_name="", _header="", _footer="for_edit_test"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(
         Group(_footer="footer_group_edit_test"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
