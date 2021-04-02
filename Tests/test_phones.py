a51f76baee70ea53c9337329d313fb93cb65f352
nprockaya <41466864+nprockaya@users.noreply.github.com>
2021-04-02 22:36:46 +0300

    Добавлен clear

--- Tests/test_phones.py
+++ Tests/test_phones.py
@@ -1,7 +1,14 @@
+import re
+
+
 def test_phones_on_homepage(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_home_page.home_phone == contact_from_edit_page.home_phone
-    assert contact_from_home_page.work_phone == contact_from_edit_page.work_phone
-    assert contact_from_home_page.mobile_phone == contact_from_edit_page.mobile_phone
-    assert contact_from_home_page.secondary_home == contact_from_edit_page.secondary_home
+    assert contact_from_home_page.home_phone == clear(contact_from_edit_page.home_phone)
+    assert contact_from_home_page.work_phone == clear(contact_from_edit_page.work_phone)
+    assert contact_from_home_page.mobile_phone == clear(contact_from_edit_page.mobile_phone)
+    assert contact_from_home_page.secondary_home == clear(contact_from_edit_page.secondary_home)
+
+
+def clear(phone_string):
+    return re.sub("[()-]", "", phone_string)
