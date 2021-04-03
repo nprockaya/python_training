cc6e9c0053b4d6810eee6f9f37f72eef6c8fd0e2
nprockaya <41466864+nprockaya@users.noreply.github.com>
2021-04-04 00:08:25 +0300

    Задание №14

--- Tests/test_validation_contact_info.py
+++ Tests/test_validation_contact_info.py
@@ -3,18 +3,20 @@ from random import randrange
 
 
 def test_phones_on_homepage(app):
-    contact_from_home_page = app.contact.get_contact_list()[0]
-    # index = randrange(len(contact_from_home_page))
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_home_page.first_name == clear(contact_from_edit_page.first_name)
-    assert contact_from_home_page.last_name == clear(contact_from_edit_page.last_name)
-    assert contact_from_home_page.address == clear(contact_from_edit_page.address)
+    contact_list = app.contact.get_contact_list()
+    index = randrange(len(contact_list))
+    contact_from_home_page = contact_list[index]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
+    assert clear(contact_from_home_page.first_name) == clear(contact_from_edit_page.first_name)
+    assert clear(contact_from_home_page.last_name) == clear(contact_from_edit_page.last_name)
+    assert clear(contact_from_home_page.address) == clear(contact_from_edit_page.address)
     assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
 
 
-def clear(phone_string):
-    return re.sub("[()-]", "", phone_string)
+def clear(contact_string):
+    contact_string_without_spaces = re.sub(r"\s+", "", contact_string)
+    return re.sub("[()-]", "", contact_string_without_spaces)
 
 
 def merge_emails_like_on_home_page(contact):
