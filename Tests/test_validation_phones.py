871381709924181698220fed03989180fbf07705
nprockaya <41466864+nprockaya@users.noreply.github.com>
2021-04-05 13:16:43 +0300

    Задание №15

--- Tests/test_validation_phones.py
+++ Tests/test_validation_phones.py
@@ -10,10 +10,10 @@ def test_phones_on_homepage(app):
 def test_phones_on_contact_view_page(app):
     contact_from_view_page = app.contact.get_contact_info_from_view_page(0)
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_view_page.home_phone == contact_from_view_page.home_phone
-    assert contact_from_view_page.work_phone == contact_from_view_page.work_phone
-    assert contact_from_view_page.mobile_phone == contact_from_view_page.mobile_phone
-    assert contact_from_view_page.secondary_home == contact_from_view_page.secondary_home
+    assert contact_from_view_page.home_phone == contact_from_edit_page.home_phone
+    assert contact_from_view_page.work_phone == contact_from_edit_page.work_phone
+    assert contact_from_view_page.mobile_phone == contact_from_edit_page.mobile_phone
+    assert contact_from_view_page.secondary_home == contact_from_edit_page.secondary_home
 
 
 def clear(phone_string):
