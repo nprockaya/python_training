beea8a0d28b760c7373d73353d5646adaf6b2df9
nprockaya <41466864+nprockaya@users.noreply.github.com>
2021-04-07 14:50:58 +0300

    Добавлен конфиг-файл

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 from Fixtures.application import Application
 import pytest
 import json
+import os.path
 
 fixture = None
 target = None
@@ -11,7 +12,8 @@ def app(request):
     global target
     browser = request.config.getoption("--browser")
     if target is None:
-        with open(request.config.getoption("--target")) as config_file:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as config_file:
             target = json.load(config_file)
     if fixture is None or not fixture.is_valid():
         fixture = Application(browser=browser, base_url=target["base_url"])
