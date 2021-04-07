ae83ec7aacc8a8796d5038fc092c7705fefffe70
nprockaya <41466864+nprockaya@users.noreply.github.com>
2021-04-07 16:10:34 +0300

    Добавлена метафункция

--- Tests/test_add_group.py
+++ Tests/test_add_group.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 from Models.group_class import Group
-from Data.add_group import constant as test_data_for_group
-import pytest
+# from Data.groups_data import constant as test_data_for_group
+# import pytest
 
 
-@pytest.mark.parametrize("group", test_data_for_group, ids=[repr(x) for x in test_data_for_group])
-def test_add_normal_group(app, group):
+# @pytest.mark.parametrize("group", test_data_for_group, ids=[repr(x) for x in test_data_for_group])
+def test_add_normal_group(app, data_groups):
+    group = data_groups
     old_groups = app.group.get_group_list()
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.count_groups()
--- conftest.py
+++ conftest.py
@@ -2,9 +2,11 @@ from Fixtures.application import Application
 import pytest
 import json
 import os.path
+import importlib
 
 fixture = None
 target = None
+
 
 @pytest.fixture()
 def app(request):
@@ -34,3 +36,14 @@ def stop(request):
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
     parser.addoption("--target", action="store", default="target.json")
+
+
+def pytest_generate_tests(metafunc):
+    for fixture in metafunc.fixturenames:
+        if fixture.startswith("data_"):
+            test_data = load_form_module(fixture[5:])
+            metafunc.parametrize(fixture, test_data, ids=[str(x) for x in test_data])
+
+
+def load_form_module(module):
+    return importlib.import_module("Data.%s" % module).test_data
