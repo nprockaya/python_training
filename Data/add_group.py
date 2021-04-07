84d5e08988cdca271b0bfa59dcea229c64a2af5a
nprockaya <41466864+nprockaya@users.noreply.github.com>
2021-04-07 15:05:36 +0300

    Вынесена Data

---
+++ Data/add_group.py
@@ -0,0 +1,24 @@
+import random
+import string
+from Models.group_class import Group
+
+constant = [
+    Group(_name="name_constant", _header="header_constant", _footer="footer_constant"),
+    Group(_name="name_constant_2", _header="header_constant_2", _footer="footer_constant_2")
+]
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " " * 10  # + string.punctuation
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+test_data_for_group = [Group(_name="", _header="", _footer="")] + \
+                      [Group(_name=random_string("group_name", 10),
+                             _header=random_string("group_name", 20),
+                             _footer=random_string("group_name", 20)) for i in range(1)]
+
+# test_data_for_group = [Group(_name=name, _header=header, _footer=footer)
+#              for name in ["", random_string("group_name", 10)]
+#              for header in ["", random_string("group_name", 20)]
+#              for footer in ["", random_string("group_name", 20)]]
+
--- Tests/test_add_group.py
+++ Tests/test_add_group.py
@@ -1,24 +1,7 @@
 # -*- coding: utf-8 -*-
 from Models.group_class import Group
-import random
-import string
+from Data.add_group import constant as test_data_for_group
 import pytest
-
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " " * 10 #+ string.punctuation
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-test_data_for_group = [Group(_name="", _header="", _footer="")] + \
-            [Group(_name=random_string("group_name", 10),
-                   _header=random_string("group_name", 20),
-                   _footer=random_string("group_name", 20)) for i in range(1)]
-
-# test_data_for_group = [Group(_name=name, _header=header, _footer=footer)
-#              for name in ["", random_string("group_name", 10)]
-#              for header in ["", random_string("group_name", 20)]
-#              for footer in ["", random_string("group_name", 20)]]
 
 
 @pytest.mark.parametrize("group", test_data_for_group, ids=[repr(x) for x in test_data_for_group])
